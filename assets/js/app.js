24644316cca57e8e08fc539915da5b24aa968de0
enka <1935915+enka@users.noreply.github.com>
2018-11-20 13:28:13 +0100

    Add delete task functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,88 +1,104 @@
 (function() {
-  const { createStore } = Redux;
+    const { createStore } = Redux;
 
-  let store;
+    let store;
 
-  const initialState = [];
+    const initialState = [];
 
-  document.addEventListener('DOMContentLoaded', event => {
-      initApp();
-  });
+    document.addEventListener('DOMContentLoaded', event => {
+        initApp();
+    });
 
-  const reducer = (state, action) => {
-      switch (action.type) {
-          case 'ADD_TASK':
-              return [...state, action.payload];
-          default:
-              return state;
-      }
-  };
-
-  function initApp() {
-      store = createStore(
-          reducer,
-          initialState,
-          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      );
-
-      const $form = document.getElementById('form');
-      $form.addEventListener('submit', event => {
-          event.preventDefault();
-          addTask(new FormData($form));
-      });
-
-      store.subscribe(handleChange);
-      render();
-  }
-
-  function addTask(data) {
-    const newId = store.getState().length;
-    const action = {
-        type: 'ADD_TASK',
-        payload: {
-            id: newId,
-            text: data.get('text'),
-            completed: false
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'ADD_TASK':
+                return [...state, action.payload];
+            case 'DELETE_TASK':
+                return state.filter(function (element) {
+                    return element.id !== action.payload.id
+                });
+            default:
+                return state;
         }
     };
 
-    store.dispatch(action);
-    clearInput();
-  }
+    function initApp() {
+        store = createStore(
+            reducer,
+            initialState,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        );
 
-  function clearInput(){
-    const $input = document.getElementById('new-todo');
-    $input.value = '';
-  }
-  
-  function handleChange() {
-    render();
-  }
+        const $form = document.getElementById('form');
+        $form.addEventListener('submit', event => {
+            event.preventDefault();
+            addTask(new FormData($form));
+        });
 
-  function render() {
-    const todos = store.getState();
-    renderTodos(todos);
-  }
+        store.subscribe(handleChange);
+        render();
 
-  function renderTodos(todos) {
-      const $container = document.getElementById('todo-list');
-      $container.innerHTML = '';
+        this.deleteTask = deleteTask.bind(this);
+    }
 
-      let todosHtml = '';
-      todos.forEach(todo => {
-          todosHtml += renderTodo(todo);
-      });
-      $container.innerHTML = todosHtml;
-  }
+    function addTask(data) {
+        const newId = store.getState().length;
+        const action = {
+            type: 'ADD_TASK',
+            payload: {
+                id: newId,
+                text: data.get('text'),
+                completed: false
+            }
+        };
 
-  function renderTodo(todo) {
-      return `
+        store.dispatch(action);
+        clearInput();
+    }
+
+    function clearInput() {
+        const $input = document.getElementById('new-todo');
+        $input.value = '';
+    }
+
+    function deleteTask(id) {
+        const action = {
+            type: 'DELETE_TASK',
+            payload: {
+                id: id
+            }
+        };
+        store.dispatch(action);
+    }
+
+    function handleChange() {
+        render();
+    }
+
+    function render() {
+        const todos = store.getState();
+        renderTodos(todos);
+    }
+
+    function renderTodos(todos) {
+        const $container = document.getElementById('todo-list');
+        $container.innerHTML = '';
+
+        let todosHtml = '';
+        todos.forEach(todo => {
+            todosHtml += renderTodo(todo);
+        });
+        $container.innerHTML = todosHtml;
+    }
+
+    function renderTodo(todo) {
+        return `
     <li data-id="${todo.id}" class="${todo.completed}">
       <div class="view">
         <input class="toggle" type="checkbox" ${todo.completed ? 'checked' : ''}>
         <label>${todo.text}</label>
-        <button class="destroy"></button>
+        <button class="destroy" onClick="deleteTask(${todo.id})"></button>
       </div>
     </li>`;
-  }
+    }
 })();
